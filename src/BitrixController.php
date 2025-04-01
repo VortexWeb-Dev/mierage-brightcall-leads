83fa5b23d995fa35416d60cfc31ef4f99558194e
fasilofficial <muhammedfasilofficial@gmail.com>
2025-04-01 13:14:58 +0530

    Add updateLead function

--- src/BitrixController.php
+++ src/BitrixController.php
@@ -47,4 +47,27 @@ class BitrixController
             return null;
         }
     }
+
+    function updateLead(array $leadData, int $leadId): ?int
+    {
+        if (empty($leadData['TITLE'])) {
+            header('Content-Type: application/json');
+            http_response_code(400);
+            echo json_encode(['error' => 'Missing required lead field: TITLE']);
+            exit;
+        }
+
+        $result = CRest::call('crm.lead.update', [
+            'id' => $leadId,
+            'fields' => $leadData,
+            'params' => ["REGISTER_SONET_EVENT" => "Y"]
+        ]);
+
+        if (isset($result['result'])) {
+            return $result['result'];
+        } else {
+            return null;
+        }
+        exit;
+    }
 }
